d3cf1bb96d18eb18e5c2bf39afda5b6e1a383478
Daniel Niknam <daniel-niknam@users.noreply.github.com>
2024-05-12 15:47:56 +1000

    Update credit for piper project and update audio location

--- post-to-audio.rb
+++ post-to-audio.rb
@@ -22,7 +22,9 @@ system(<<-BASH)
 title=$(cat #{post_file_path} | grep "title:" | awk '{gsub("title: ", "", $0); print}')
 description=$(cat #{post_file_path} | grep "description:" | awk '{gsub("description: ", "", $0); print}')
 content=$(pandoc -f markdown -t plain --wrap=none #{post_file_path})
-credit="Thanks for listening. Title of this post is $title, $description, and is licensed under CC BY-SA. Read and listen to more content in my website: niknam.org.\nThis audio is generated using Piper voice generator project. Background music is from Calm Morning by PianoAmor from pixabay.com."
+credit="Thanks for listening. Title of this post is $title, $description, and is licensed under CC BY-SA 4.0. Read and listen to more content in my website: niknam.org.\nThis audio is generated using the Piper voice generator project from github.com/rhasspy/piper. The background music is Calm Morning by PianoAmor from pixabay.com."
+
+mkdir -p assets/audio/posts
 
 echo "$title.\n$description.\n$content\n$credit" \
   | awk '{gsub("{.*}", "", $0); print}' \
@@ -30,7 +32,7 @@ echo "$title.\n$description.\n$content\n$credit" \
   | piper \
     --model ~/Projects/piper-voices/en_US-hfc_female-medium.onnx \
     --config ~/Projects/piper-voices/en_US-hfc_female-medium.onnx.json \
-    --output_file assets/posts/#{post_file_without_ext}.wav
+    --output_file assets/audio/posts/#{post_file_without_ext}.wav
 BASH
 
 puts
@@ -39,12 +41,12 @@ puts
 
 system(<<-BASH)
 ffmpeg -y \
-  -i assets/posts/#{post_file_without_ext}.wav \
+  -i assets/audio/posts/#{post_file_without_ext}.wav \
   -stream_loop -1 -i ~/Projects/music/calm/calm-morning-125568.mp3 \
   -filter_complex "[1:a]adelay=1000|1000:all=1[bg];[0:a][bg]amix=inputs=2:duration=shortest:dropout_transition=2:weights=1 0.3"  \
   -c:a libmp3lame \
   -q:a 4 \
-  assets/posts/#{post_file_without_ext}-with-music.mp3
+  assets/audio/posts/#{post_file_without_ext}-with-music.mp3
 BASH
 
 puts
@@ -53,9 +55,9 @@ puts
 
 system(<<-BASH)
 ffmpeg -y \
-  -i assets/posts/#{post_file_without_ext}-with-music.mp3 \
+  -i assets/audio/posts/#{post_file_without_ext}-with-music.mp3 \
   -vn -ar 44100 -ac 2 -b:a 96k \
-  assets/posts/#{post_file_without_ext}.mp3
+  assets/audio/posts/#{post_file_without_ext}.mp3
 BASH
 
 puts
@@ -64,8 +66,8 @@ puts
 
 
 system(<<-BASH)
-rm assets/posts/#{post_file_without_ext}-with-music.mp3 \
-  && rm assets/posts/#{post_file_without_ext}.wav
+rm assets/audio/posts/#{post_file_without_ext}-with-music.mp3 \
+  && rm assets/audio/posts/#{post_file_without_ext}.wav
 BASH
 
 puts
